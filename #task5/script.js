4aea2badcbac6bad93a6250e6d2512d2085884a2
kiranfarswan <97655612+kiranfarswan@users.noreply.github.com>
2025-02-22 12:54:47 +0530

    added

---
+++ #task5/script.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector(".menu-icon");
+const menu_header = document.querySelector(".header");
+const toggleMenu = () => {
+    menu_header.classList.toggle("active");
+   
+};
+
+menu.addEventListener('click', () => toggleMenu());
